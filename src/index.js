6d53c45ab001ee7b197ddef427b6477f0610b011
Trommelochse <clemens.a.janes@gmail.com>
2019-01-24 20:56:29 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import Sandbox from './Sandbox';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
