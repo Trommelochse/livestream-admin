6a4e690a38b5b9458e0ba1cd3d6ee3669fd376a9
Trommelochse <clemens.a.janes@gmail.com>
2019-01-24 20:55:19 +0100

    Update isa.js

--- src/isa.js
+++ src/isa.js
@@ -1,5 +1,5 @@
 export const leagueInfo = scid => {
-  const ocb = process.env.REACT_APP_ISA_OCB;
+  const ocb = process.env.REACT_APP_ISA_OCB || '7ddb8793-e408-4da4-a3d0-09ce59b61d2d';
   const api = `https://bts-api-a.bpsgameserver.com/isa/v2/901/en/event`;
   const params = `?ocb=${ocb}&subCategoryIds=${scid}&EventMarketCount=1`;
   return fetch(`${api}${params}`)
